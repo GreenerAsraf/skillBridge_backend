8338a010c8b879c303346764fa5fa4b182a6a9d2
Asraf <asraf.uddin4407@gmail.com>
2026-06-11 23:15:29 +0600

    added solution to my messed up

--- dist/src/lib/prisma.js
+++ dist/src/lib/prisma.js
@@ -5,7 +5,6 @@ require("dotenv/config");
 const pg_1 = require("pg");
 const adapter_pg_1 = require("@prisma/adapter-pg");
 const client_1 = require("../../generated/prisma/client");
-// import { PrismaClient } from '../generated/prisma/client'
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new pg_1.Pool({ connectionString });
 const adapter = new adapter_pg_1.PrismaPg(pool);
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,6 @@ import 'dotenv/config'
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../../generated/prisma/client'
-// import { PrismaClient } from '../generated/prisma/client'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
