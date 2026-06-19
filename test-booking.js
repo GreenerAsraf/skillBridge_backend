df9ecdfff37ffa65a859f2bcdccf34092b062fd3
Asraf <asraf.uddin4407@gmail.com>
2026-06-19 22:09:47 +0600

    added test booking

--- test-booking.js
+++ test-booking.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 async function test() {
-  const res = await fetch('http://localhost:5000/api/bookings', {
+  const res = await fetch('https://skillbridgebackend-production-19ba.up.railway.app/api/bookings', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
