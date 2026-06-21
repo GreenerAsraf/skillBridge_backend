93232499ec92885430c8b7de469df703ed7ab6bf
Asraf <asraf.uddin4407@gmail.com>
2026-06-21 22:36:58 +0600

    remove dist from git tracking

--- dist/modules/User/user.validation.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// import { z } from 'zod';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userValidationSchema = void 0;
-exports.userValidationSchema = {
-// Add validation schemas here
-};
--- dist/modules/tutor/tutor.constant.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TutorSearchableFields = void 0;
-exports.TutorSearchableFields = [
-// Add searchable fields here
-];
