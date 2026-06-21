99df0060736c6d5270ede19d9ce241a467b79c1e
Asraf <asraf.uddin4407@gmail.com>
2026-06-21 22:17:01 +0600

    update dependencies

--- dist/lib/prisma.js
+++ dist/lib/prisma.js
@@ -5,9 +5,9 @@ require("dotenv/config");
 const pg_1 = require("pg");
 const adapter_pg_1 = require("@prisma/adapter-pg");
 // import { PrismaClient } from '../../generated/prisma/client'
-const prisma_1 = require("../generated/prisma");
+const client_1 = require("@prisma/client");
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new pg_1.Pool({ connectionString });
 const adapter = new adapter_pg_1.PrismaPg(pool);
-const prisma = new prisma_1.PrismaClient({ adapter });
+const prisma = new client_1.PrismaClient({ adapter });
 exports.prisma = prisma;
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 // import { PrismaClient } from '../../generated/prisma/client'
 
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`
 
